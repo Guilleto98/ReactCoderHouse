8ac7ac2fc58c4e91b10f8e4cf837c4bb4383f126
GuillermoTo <guillermo98td@gmail.com>
2022-05-14 19:20:14 -0300

    Changed File Location

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import NavBar from './NavBar'
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Coderhouse </h1>
+      <NavBar />
+    </div>
+  );
+}
+
+export default App;
